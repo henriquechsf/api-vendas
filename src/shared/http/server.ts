1de0a670f8cd61552aaf3436e721550cc37e6981
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-08-15 18:59:17 -0300

    implemented validations for request HTTP on route of products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -11,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+// metodo do celebrate para validar as requisições HTTP
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
