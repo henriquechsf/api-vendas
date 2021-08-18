15214efdbc78b90e7f518a2577351b499fd23dd1
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-08-17 22:20:11 -0300

    generate token response authentication

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -9,12 +10,13 @@ interface IRequest {
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
 
     const user = await usersRepository.findByEmail(email);
@@ -29,7 +31,16 @@ class CreateSessionsService {
       throw new AppError('Invalid user/password.');
     }
 
-    return user;
+    // hash md5
+    const token = sign({}, 'db9d24ef0cc5b7d08ca6bc88b128c227', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
