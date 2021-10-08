d0e8b4136f6be710ce0e549a8fe5972f1c623fdc
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-10-08 13:11:10 -0300

    add environments variables

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
@@ -21,7 +22,7 @@ app.use(routes);
 // metodo do celebrate para validar as requisições HTTP
 app.use(errors());
 
-app.use(
+/*app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
@@ -35,7 +36,7 @@ app.use(
       message: 'Internal server error',
     });
   },
-);
+);*/
 
 const PORT = 3333;
 app.listen(PORT, () => {
