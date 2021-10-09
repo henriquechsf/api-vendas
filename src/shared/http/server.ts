b326241ded8cb7b4c2822f6eebd93e9a1b012715
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-10-08 22:06:27 -0300

    add root route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -22,7 +22,7 @@ app.use(routes);
 // metodo do celebrate para validar as requisições HTTP
 app.use(errors());
 
-/*app.use(
+app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
@@ -36,7 +36,7 @@ app.use(errors());
       message: 'Internal server error',
     });
   },
-);*/
+);
 
 const PORT = 3333;
 app.listen(PORT, () => {
