18b7881e51dc5cdf023bbe74a6c190645b0f6a2e
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-10-12 15:01:16 -0300

    refact: create session and forgot password

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -25,7 +25,7 @@ app.use(routes);
 // metodo do celebrate para validar as requisições HTTP
 app.use(errors());
 
-/*app.use(
+app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
@@ -39,7 +39,7 @@ app.use(errors());
       message: 'Internal server error',
     });
   },
-);*/
+);
 
 const PORT = 3333;
 app.listen(PORT, () => {
